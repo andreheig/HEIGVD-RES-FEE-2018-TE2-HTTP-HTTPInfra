6c230b80e102e92ee3eb3a3815bd91c5c3d13fc5
andre <andre.jacquemond@heig-vd.ch>
2018-06-01 21:16:04 +0200

    test script

--- ImagesDockers/apache_reverse-proxy/reverse-proxy-launcher.php
+++ ImagesDockers/apache_reverse-proxy/reverse-proxy-launcher.php
@@ -1,20 +1,52 @@
 <?php 
+	$DOCKER_IMAGE_STATIC_SHARED="static_shared";
+	$DOCKER_IMAGE_EXPRESS="express";
+	$DOCKER_IMAGE_REVERSE_PROXY="reverse-proxy";
+	$DOCKER_ARG_VARIABLE="-e";
+	$STATIC_APP="STATIC_APP";
+	$DYNAMIC_APP="DYNAMIC_APP";
+	$STATIC_APP_PORT=":80";
+	$DYNAMIC_APP_PORT=":3000";
+
 	echo "try to get ip address\n";
-	#$tab = array(); 
-	#$static_app_name = explode(' ', exec("docker ps -f ancestor=testip |  cut -d "/" -f2 | cut -c 6-40 | tail -n +2"));
-	$static_app_name = shell_exec("docker ps -q -f ancestor=testip");
-	#echo "$static_app_name";
+
+	$static_app_name = shell_exec("docker ps -q -f ancestor=". $DOCKER_IMAGE_STATIC_SHARED);
+
 	$tab = explode("\n", $static_app_name);
 
+	$array_static_ip = array();
+	$array_dynamic_ip = array();
 	foreach ($tab as $name) {
-		echo "$name";
-		$static_ip = exec("docker inspect " . $name . " | grep IPAddress | grep -E \b([0-9]{1,3}.){3}[0-9]{1,3}\b");
-		echo "$static_ip\n";
+		if(strlen($name) > 1){
+			$static_ip = exec("docker inspect " . $name . " | grep IPAddress | cut -d \":\" -f2 | cut -d \"\"\" -f2");
+			$array_static_ip[] = $static_ip;
+		}
 	}
 
-	#exec("docker ps -f ancestor=testip |  cut -d "/" -f2 | cut -c 6-40 | tail -n +2", $tab );
-	#$static_app_name = exec("docker ps -f ancestor=testip |  cut -d "/" -f2");
-	#$static_app_name = exec("docker ps -f ancestor=testip |  cut -d "/" -f2 | cut -c 6-40");
-	#$static_app_name = exec("docker ps -f ancestor=testip |  cut -d "/" -f2 | cut -c 6-40 | tail -n +2");
-	#| grep -E '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'
+	$dynamic_app_name = shell_exec("docker ps -q -f ancestor=" . $DOCKER_IMAGE_EXPRESS);
+	#echo "$static_app_name";
+	$tab = explode("\n", $dynamic_app_name);
+
+	foreach ($tab as $name) {
+		if(strlen($name)>1){
+			$dynamic_ip = exec("docker inspect " . $name . " | grep IPAddress | cut -d \":\" -f2 | cut -d \"\"\" -f2");
+			$array_dynamic_ip[] = $dynamic_ip;
+		}
+	}
+	$index = 0;
+	#$argument = "";
+	foreach($array_static_ip as $name){
+			#echo "$name\n";
+			$argument = $argument . "" . $DOCKER_ARG_VARIABLE . " " . $STATIC_APP . "" . $index . "=" . $name . "" . $STATIC_APP_PORT . " ";
+			$index++;
+	}
+	foreach($array_dynamic_ip as $name){
+			#echo "$name\n";
+			$argument = $argument . "" . $DOCKER_ARG_VARIABLE . " " . $DYNAMIC_APP . "" . $index . "=" . $name . "" . $DYNAMIC_APP_PORT . " ";
+			$index++;
+	}
+	echo "$argument";
+
+	exec("docker run -d -p 8080:80 " . $argument . " reverse-proxy");
+
 ?>
--- ImagesDockers/apache_reverse-proxy/templates/config-template.php
+++ ImagesDockers/apache_reverse-proxy/templates/config-template.php
@@ -1,12 +1,27 @@
 <?php 
-	$static_app = getenv('STATIC_APP');
-	$dynamic_app = getenv('DYNAMIC_APP');
+	$DYNAMIC_APP="DYNAMIC_APP";
+    $STATIC_APP="STATIC_APP";
+    $tab = shell_exec("printenv | grep APP");
+    $tableau = explode("\n", $tab);
+    foreach($tableau as $name){
+            if(strlen($name)>1){
+                    if(strpos($name, $STATIC_APP) !== false){
+                            echo "static: " . "$name\n";
+                            system('cat $name >> /etc/apache2/sites-available/001-reverse-proxy.conf');
+                    }
+                    else if(strpos($name, $DYNAMIC_APP) !== false){
+                            echo "dynamic: " . "$name\n";
+                            system('cat $name >> /etc/apache2/sites-available/001-reverse-proxy.conf');
+                    }
+            }
+    }
 ?>
 
 <VirtualHost *:80>
 #Défini notre nom de domaine
 	ServerName www.reverse-proxy.res.ch
-
+<Proxy "balancer://static">
+	BalanceMember "http://<"
 # Partie dynamique => Kite
 	# Permet de traiter quand on arrive
 	ProxyPass '/api/kiteplace/' 'http://<?php print "$dynamic_app"?>/' 
