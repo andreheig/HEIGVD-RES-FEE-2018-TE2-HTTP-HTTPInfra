01d65cf8837ea77e3970219d439e8b994e097382
andre <andre.jacquemond@heig-vd.ch>
2018-06-07 23:17:20 +0200

    Blocage avec get api docker

--- dockerManager/src/app/app.component.ts
+++ dockerManager/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-docker',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Docker Administration';
+  constructor(private http: HttpClient){
+  }
+  getImage(){
+  	console.log("hello");
+  	this.http.get("http://localhost:2375/").subscribe(data => {
+      console.log(data);
+    });
+  }
+  }
 }
+
